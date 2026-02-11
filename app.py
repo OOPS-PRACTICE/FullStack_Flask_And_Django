42375eebc45598d1e64151e19d86f96fc5598cfa
OOPS-PRACTICE <gurukripacreations25@gmail.com>
2026-02-11 11:00:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from flask_smorest import abort
 app = Flask(__name__)
 
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
 @app.post("/store")
 def addStoreDetail():
 
